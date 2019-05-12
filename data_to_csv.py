21c6f1a5ec1c878ab639154a13be78a40ab8127c
Samuel <sam.o.bryson@gmail.com>
2019-05-12 21:42:56 +0200

    csv converter finished

--- data_to_csv.py
+++ data_to_csv.py
@@ -24,7 +24,6 @@ users = os.listdir('user_data')
 for user in users:
     try:
         assert user != 'current_user.txt'
-        print(user)
         with open('user_data/{}'.format(user), 'r') as user_file:
             for key in json.load(user_file).keys():
                 title_line.append(key)
@@ -33,13 +32,11 @@ for user in users:
         continue
 
 
-# Delete previous data
 with open('questionnaire.csv'.format(user), 'w') as user_file:
     row_writer = csv.writer(user_file, delimiter=',')
     row_writer.writerow(title_line)
     for user in users:
         data_line = []
-        print(user)
         data_line.append(user.split('.')[0])
         with open('user_data/{}'.format(user), 'r') as user_file:
             user_data = json.load(user_file)
@@ -52,65 +49,7 @@ with open('questionnaire.csv'.format(user), 'w') as user_file:
         except AttributeError:
             continue
         for key in user_data:
-            if key == 'user_data':
-                user_data_section = user_data[key]
-                continue
             data_line.append(user_data[key])
-            for key in user_data_section:
-                data_line.append(user_data_section[key])
 
         row_writer.writerow(data_line)
         data_line = []
-
-
-
-
-
-
-#     with open('program_data/{}.txt'.format(key)) as f:
-#         length = len(f.readlines())
-#     orig_key = key
-#     for i in range(length):
-#         i += 14 if orig_key == 'ZMA29_2' else 0
-#         item_list.append(orig_key + '_' +  str('{:02d}'.format(i+1)))
-#         key = orig_key[:-2] if orig_key[:3] == 'ZMA' else key
-#         title_line.append(key + '_' +  str('{:02d}'.format(i+1)))
-#         if orig_key[:3] == 'GAD' or orig_key[:3] == 'PHQ':
-#             coding_adjust.append(1)
-#         else:
-#             coding_adjust.append(0)
-
-# write_line(title_line)
-
-# user_files = os.listdir('user_data/')
-# users = [user.split('.')[0] for user in user_files]
-
-# for uid in users:
-
-#     print('Processing user {}...'.format(uid))
-
-#     # Retrieve data
-#     try:
-#         with open('user_data/{}.json'.format(uid), 'r') as f:
-#             data = json.load(f)
-#     except FileNotFoundError:
-#         print('User \'{}\' ignored.'.format(uid))
-#         continue
-
-#     line = [uid]
-
-#     # Format data for writing
-#     for item in item_list:
-#         try:
-#             line.append(data[item])
-#         except KeyError:
-#             line.append('NA')
-
-#     # Coding correction
-#     for i in range(len(line)):
-#         try:
-#             line[i] -= coding_adjust[i]
-#         except TypeError:
-#             continue
-
-#     write_line(line)
